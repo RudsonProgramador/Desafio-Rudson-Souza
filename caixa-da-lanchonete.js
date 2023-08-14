633868a2975e403fd847f472601baf71b8674bab
RudsonProgramador <133825062+RudsonProgramador@users.noreply.github.com>
2023-08-14 00:41:08 -0300

    Update caixa-da-lanchonete.js

--- caixa-da-lanchonete.js
+++ caixa-da-lanchonete.js
@@ -1,20 +1,20 @@
 class CaixaDaLanchonete {
     constructor() {
       this.cardapio = {
-        cafe: { descricao: "Café", valor: 3.0 },
-        chantily: { descricao: "Chantily (extra do Café)", valor: 1.5 },
+        cafe: { descricao: "Cafe", valor: 3.0 },
+        chantily: { descricao: "Chantily (extra do Cafe)", valor: 1.5 },
         suco: { descricao: "Suco Natural", valor: 6.2 },
-        sanduiche: { descricao: "Sanduíche", valor: 6.5 },
-        queijo: { descricao: "Queijo (extra do Sanduíche)", valor: 2.0 },
+        sanduiche: { descricao: "Sanduiche", valor: 6.5 },
+        queijo: { descricao: "Queijo (extra do Sanduiche)", valor: 2.0 },
         salgado: { descricao: "Salgado", valor: 7.25 },
-        combo1: { descricao: "1 Suco e 1 Sanduíche", valor: 9.5 },
-        combo2: { descricao: "1 Café e 1 Sanduíche", valor: 7.5 },
+        combo1: { descricao: "1 Suco e 1 Sanduiche", valor: 9.5 },
+        combo2: { descricao: "1 Cafe e 1 Sanduiche", valor: 7.5 },
       };
     }
   
     calcularValorDaCompra(formaDePagamento, itens) {
       if (itens.length === 0) {
-        return "Não há itens no carrinho de compra!";
+        return "Não ha itens no carrinho de compra!";
       }
   
       let valorTotal = 0;
@@ -23,7 +23,7 @@ class CaixaDaLanchonete {
         const [codigo, quantidade] = itemPedido.split(",");
   
         if (!this.cardapio[codigo] || isNaN(quantidade) || parseInt(quantidade) <= 0) {
-          return "Item inválido!";
+          return "Item invalido!";
         }
   
         valorTotal += this.cardapio[codigo].valor * parseInt(quantidade);
@@ -44,7 +44,7 @@ class CaixaDaLanchonete {
         case "debito":
           break; // Nenhuma modificação para pagamento a débito
         default:
-          return "Forma de pagamento inválida!";
+          return "Forma de pagamento invalida!";
       }
   
       // Arredondar para duas casas decimais
